c3b6bb1773074c685393f463fabad6525ffca42e
Haram0111 <5gkfka5@naver.com>
2023-08-24 02:52:51 +0900

    Token 사용방법

--- src/js/userToken.jsx
+++ src/js/userToken.jsx
@@ -2,4 +2,6 @@
 // import { useSelector } from 'react-redux';
 
 // const token = useSelector(state => state.token);
-// const accessToken = "Bearer "+ {token};
+// const accessToken = `Bearer ${token}`;
+
+//useEffect 안에 넣지 말고 페이지 함수 ex) home() 바로 밑에 적어야 ReactHook 안나는 것 같아요 
