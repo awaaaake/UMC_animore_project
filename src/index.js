e795d78f84ecd55529295917fab808dceaec5b12
choyunhee <xornd1110@naver.com>
2023-08-23 17:52:30 +0900

    index.js 주석 해제

--- src/index.js
+++ src/index.js
@@ -1,46 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
+import store from './store'; // Redux 스토어 임포트
+
+axios.defaults.baseURL = 'https://animore.co.kr';
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <CookiesProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CookiesProvider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <CookiesProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CookiesProvider>
+    </React.StrictMode>
+  </Provider>
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import axios from "axios";
-// import { Provider } from 'react-redux';
-// import store from './store'; // Redux 스토어 임포트
-
-// axios.defaults.baseURL = "https://animore.co.kr";
-// axios.defaults.withCredentials = true;
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <Provider store={store}>
-//         <App />
-//   </Provider>,
-//     document.getElementById('root')
-// );
